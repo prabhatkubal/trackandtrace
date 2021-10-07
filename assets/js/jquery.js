03a4fc20746d02f5170ad1c7a0a9291ab3c1bb0b
prabhatkubal <65513184+prabhatkubal@users.noreply.github.com>
2021-10-08 02:27:57 +0530

    ..
    ..

---
+++ assets/js/jquery.js
@@ -0,0 +1,60 @@
+// Script for progress bar on viewdetails.html - page
+$('.steps').on('click', '.step--active', function () {
+  $(this).removeClass('step--incomplete').addClass('step--complete');
+  $(this).removeClass('step--active').addClass('step--inactive');
+  $(this).next().removeClass('step--inactive').addClass('step--active');
+});
+
+$('.steps').on('click', '.step--complete', function () {
+  $(this).removeClass('step--complete').addClass('step--incomplete');
+  $(this).removeClass('step--inactive').addClass('step--active');
+  $(this).nextAll().removeClass('step--complete').addClass('step--incomplete');
+  $(this).nextAll().removeClass('step--active').addClass('step--inactive');
+});
+
+// Script for pop-up on viewdetails.html - page
+$('.customer-order-details').on('click', function () {
+  $('nav').css({ 'z-index': 0 });
+  $('.pop-up-bg-mask').css("display", "block");
+  $('.sh-col1').css({ 'z-index': -1 });
+  $('.sh-col2').css("display", "block");
+  $('.sh-ord-details').css("display", "block");
+  $('.sh-cus-info').css("display", "block");
+  $('.sh-col2-cta1').css("display", "flex");
+  $('.sh-col2-cta2').css("display", "flex");
+});
+
+$('.sh-col2-X').on('click', function () {
+  $('nav').css({ 'z-index': 999 });
+  $('.pop-up-bg-mask').css("display", "none");
+  $('.sh-col1').css({ 'z-index': 0 });
+  $('.sh-col2').css("display", "none");
+  $('.sh-ord-details').css("display", "none");
+  $('.sh-cus-info').css("display", "none");
+  $('.sh-col2-cta1').css("display", "none");
+  $('.sh-col2-cta2').css("display", "none");
+});
+
+// Script for date-picker on viewdetails.html - page
+
+// const monthNames = ["January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"
+// ];
+
+const today = new Date()
+const currentMonth = today.toLocaleString('default', { month: 'long' });
+
+$('.dt-month').html(today.toLocaleString('default', { month: 'long' }));
+$('.dt-year').html(today.getFullYear());
+
+$('.dl-chng-date').on('click', function () {
+  $('.custom-date-picker').css("display","flex");
+  $('.sh-col2').css("display", "none");
+  $('.pop-up-bg-mask').css("opacity", 0.6);
+})
+
+$('.sh-col3-X').on('click', function () {
+  $('.custom-date-picker').css("display", "none");
+  $('.sh-col2').css("display", "block");
+  $('.pop-up-bg-mask').css("opacity", 0.3);
+});
