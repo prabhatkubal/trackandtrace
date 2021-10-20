68592a08615aaae8e4b48fd559879834a114e883
prabhatkubal <65513184+prabhatkubal@users.noreply.github.com>
2021-10-21 01:54:57 +0530

    .Calender
    The Calender code is in the app.js folder

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,151 @@
 const navAnimation = () => {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav-ul');
-    const navItems = document.querySelectorAll('.navlinks');
-    const linkSubnav = document.querySelector('.link-subnav');
-    const subNav = document.querySelector('.sub-nav');
-    const subNavItems = document.querySelectorAll('.sub-navitems');
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav-ul');
+  const navItems = document.querySelectorAll('.navlinks');
+  const linkSubnav = document.querySelector('.link-subnav');
+  const subNav = document.querySelector('.sub-nav');
+  const subNavItems = document.querySelectorAll('.sub-navitems');
 
-    hamburger.addEventListener('click', () => {
-        // Toggle Class
-        nav.classList.toggle('nav-view');
+  hamburger.addEventListener('click', () => {
+    // Toggle Class
+    nav.classList.toggle('nav-view');
 
-        // Nav bar items Animation
-        navItems.forEach((item, index) => {
-            if(item.style.animation) {
-                item.style.animation = ''
-            } else {
-                item.style.animation = `navFade 0.5s ease forwards ${index / 9 + 0.45}s`
-            }
-        });
-
-        hamburger.classList.toggle('toggle');
+    // Nav bar items Animation
+    navItems.forEach((item, index) => {
+      if (item.style.animation) {
+        item.style.animation = ''
+      } else {
+        item.style.animation = `navFade 0.5s ease forwards ${index / 9 + 0.45}s`
+      }
     });
 
-    linkSubnav.addEventListener('click', () => {
-        subNav.classList.toggle('subnav-view');
+    hamburger.classList.toggle('toggle');
+  });
 
-        subNavItems.forEach((item, index) => {
-            if(item.style.animation) {
-                item.style.animation = ''
-            } else {
-                item.style.animation = `navFade 0.5s ease forwards ${index / 8 + 0.8}s`
-            }
-        });
-    })
+  linkSubnav.addEventListener('click', () => {
+    subNav.classList.toggle('subnav-view');
+
+    subNavItems.forEach((item, index) => {
+      if (item.style.animation) {
+        item.style.animation = ''
+      } else {
+        item.style.animation = `navFade 0.5s ease forwards ${index / 8 + 0.8}s`
+      }
+    });
+  })
 }
 
 navAnimation();
+
+
+//Script For Calender And Date Picker Pop Up On Viewdetails.html Page
+
+let yearChosen = new Date().getFullYear(); //current year
+let monthChosen = new Date().getMonth(); //current month 0 to 11 where 0 = january & 11 = december
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+function getNumberOfDays(year, month) {
+  let numDays = new Date(year, month + 1, 0).getDate();
+  return numDays;
+}
+
+function renderCal(getNumDays, newYear, newMonth) {
+  let monthName = months[newMonth]
+  let yearPTag = document.getElementById('year');
+  yearPTag.innerHTML = newYear;
+  let monthPTag = document.getElementById('month');
+  monthPTag.innerHTML = monthChosen;
+
+  // let DateToDisplay = document.getElementById('label-date');
+  // DateToDisplay.innerHTML = monthName + yearChosen;
+
+  let dayColumns = document.getElementsByClassName('day-column');
+  for (let otherIndex = 0; otherIndex < dayColumns.length; otherIndex++) {
+    dayColumns[otherIndex].innerHTML = "";
+  }
+
+  let firstDay = monthName + ' 1,' + newYear;
+  let firstDayOfMonth = new Date(firstDay).getDay();
+  for (let anotherIndex = 0; anotherIndex < firstDayOfMonth; anotherIndex++) {
+    console.log(anotherIndex);
+    let blankPTag = document.createElement("p");
+    let blankText = document.createTextNode(" ");
+    blankPTag.style.padding = '16px';
+    blankPTag.appendChild(blankText);
+    let dayColumn = document.getElementById(anotherIndex.toString());
+    dayColumn.appendChild(blankPTag);
+  }
+
+
+  for (let i = 1; i <= getNumDays; i++) {
+    let dayPTag = document.createElement("p");
+    dayPTag.style.fontSize = "20px";
+    let dayText = document.createTextNode(i.toString());
+    dayPTag.appendChild(dayText);
+    let date = monthName + " " + i.toString() + ", " + newYear;
+    //console.log(date);
+    let dayOfWeek = new Date(date).getDay();
+    //console.log(dayOfWeek);
+    document.getElementById(dayOfWeek.toString()).appendChild(dayPTag);
+  }
+}
+
+// let selectedMonth = document.getElementById('selectedMonth');
+
+// function changeMonth() {
+//     if(selectedMonth != null) {
+//       monthChosen = selectedMonth.value;
+//     }
+//     console.log(selectedMonth.value);
+
+//     renderCal(getNumberOfDays(yearChosen, monthChosen));
+// }
+
+function changeMonth(addMinus) {
+  if (addMinus === "minus") {
+    if (monthChosen !== 0) {
+      monthChosen -= 1;
+      renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
+    }
+    else {
+      monthChosen = 11;
+      yearChosen -= 1;
+      renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
+    }
+  } else {
+    if (monthChosen !== 11) {
+      monthChosen += 1;
+      renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
+    }
+    else {
+      monthChosen = 0;
+      yearChosen += 1;
+      renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
+    }
+  }
+}
+
+function changeYear(addMinus) {
+  if (addMinus === "minus") {
+    yearChosen -= 1;
+    renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
+  } else {
+    yearChosen += 1;
+    renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
+  }
+}
+
+renderCal(getNumberOfDays(yearChosen, monthChosen), yearChosen, monthChosen);
--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -56,73 +56,399 @@ $('.sh-col3-X').on('click', function () {
 
 //Script For Calender And Date Picker Pop - Up on viewdetails.html page
 
+/*
+Vanilla Javascript Datepicker
+Designed by: Dan Boswell
+*/
 
-let yearChosen = new Date().getFullYear();
-let monthChosen = new Date().getMonth();
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
+// var monthsToShow, primary, accent, mode;
+// var cal;
+// const d = new Date();
+// var year = d.getFullYear();
+// var month = d.getMonth() - 1;
+// const calendar = document.getElementsByClassName("calendar")[0];
+// const calContainer = document.getElementsByClassName("cal__container")[0];
+// let startInput = document.getElementById("start");
+// let endInput = document.getElementById("end");
+// const inputContainer = document.getElementsByClassName('inputs')[0];
+// const clearContainer = document.getElementsByClassName('clear__btn')[0];
+// var dateRange = [];
+// var style = false;
+// const months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "Decemeber"
+// ];
 
-function getNumberOfDays(year, month) {
-  let numDays = new Date(year, month + 1, 0).getDate();
-  return numDays;
-}
+// const disabledDays = [
+//   "23/03/2020"
+// ];
 
-function renderCal(getNumDays) {
-  let DateToDisplay = document.getElementById('label-date');
-  let monthName = months[monthChosen]
-  DateToDisplay.innerHTML = monthName + yearChosen;
+// Pikka = {
+//   date: function(options){
+//     style = options.style;
+//     monthsToShow = options.monthsToRender;
+//     accent = options.accentColor;
+//     primary = options.primaryColor;
+//     mode = options.mode;
+//   }
+// }
 
-  let dayColumns = document.getElementsByClassName('day-column');
-  for (let x = 1; x < dayColumns.length; x++) {
-    dayColumns[x].innerHTML = "";
-  }
+// // options for setting up the calendar
+// Pikka.date({
+//   format: "dd/mm/yyyy",
+//   style: "circles", // squares or circles
+//   primaryColor: "#FF5A5F",
+//   accentColor: "#f99092",
+//   monthsToRender: 4, // how many months to render in multiple mode
+//   disabledDatesFormat: "en-GB",
+//   userDatesFormat: "en-GB",
+//   mode: "multiple" // single or multiple
+// });
 
-  let firstDay = monthName + ' 1,' + yearChosen;
-  let firstDayOfMonth = new Date(firstDay).getDay();
-  for (let y = 1; y < firstDayOfMonth; y++) {
-    let blankPTag = document.createElement("p");
-    let blankText = document.createTextNode(" ");
-    blankPTag.style.padding = '12px';
-    blankPTag.appendChild(blankText);
-    let dayColumn = document.getElementById(y.toString());
-    dayColumn.appendChild(blankPTag);
-  }
+// document.addEventListener("DOMContentLoaded", function() {
+//   if(mode == "single") {
+//     document.getElementsByClassName('mobile')[0].classList.add('single');
+//     monthsToShow = 1;
+//   } else {
+//     monthsToShow = 4;
+//   }
+//   for (let i = 1; i <= monthsToShow; i++) {
+//     month = month + 1;
+//     if (month == 12) {
+//       month = 0;
+//       year = year + 1;
+//     }
+//     buildCal();
+//   }
+//   if(mode == "multiple") {
+//     addMoreMonths(); 
+//   }
+// });
 
+// // function to destroy the calendar element
+// function clearCal() {
+//   let calCont = document.getElementsByClassName('cal__container')[0];
+//   calCont.innerHTML = "";
+// }
 
-  for (let i = 1; i <= getNumDays; i++) {
-    let dayPTag = document.createElement("p");
-    dayPTag.style.fontSize = "18px";
-    let dayText = document.createTextNode(i.toString());
-    dayPTag.appendChild(dayText);
-    let date = monthName + " " + i.toString() + ", " + yearChosen;
-    //console.log(date);
-    let dayOfWeek = new Date(date).getDay();
-    //console.log(dayOfWeek);
-    document.getElementById(dayOfWeek.toString()).appendChild(dayPTag);
-  }
-}
+// function nextMonth() {
+//   clearCal();
+//   month = month + 1;
+//   if (month > 11) {
+//       month = 0;
+//       year = year + 1;
+//   }
+//   buildCal();
+//   colourDates();
+// }
 
-let selectedMonth = document.getElementById('selectedMonth');
+// function prevMonth() {
+//   clearCal();
+//   if (month == 0) {
+//       month = 11;
+//       year = year - 1;
+//   } else {
+//    month = month - 1; 
+//   }
+//   buildCal();
+//   colourDates();
+// }
 
-function changeMonth() {
-    if(selectedMonth != null) {
-      monthChosen = selectedMonth.value;
-    }
-    console.log(selectedMonth.value);
+// // Logic for calendar
+// function buildCal() {
+//   let monthControls = document.createElement("div");
+//   monthControls.classList.add('month__controls');
+//   let newMonth = document.createElement("div");
+//   let monthTitle = document.createElement("span");
+//   let monthText = document.createTextNode(months[month] + " " + year);
+//   let rArrow,lArrow;
+//   newMonth.appendChild(monthControls);
   
-    renderCal(getNumberOfDays(yearChosen, monthChosen));
-}
+//   //single mode needs next/prev month controls
+//   if(mode == "single") {
+//     rArrow = document.createElement('div');
+//     lArrow = document.createElement('div');
+//     lArrow.innerHTML = '<i class="fas fa-arrow-left"></i>';
+//     lArrow.classList.add('lArrow');
+//     rArrow.innerHTML = '<i class="fas fa-arrow-right"></i>';
+//     rArrow.classList.add('rArrow');
+//     rArrow.addEventListener('click', nextMonth);
+//     lArrow.addEventListener('click', prevMonth);
+//   }
+//   monthTitle.appendChild(monthText);
+//   newMonth.classList.add("month");
+  
+//   // create the table in the DOM
+//   cal = createTable();
+//   const daysOfWeek = ["m", "t", "w", "t", "f", "s", "s"];
+//   let header = cal.createTHead();
+//   for (let i = 0; i < daysOfWeek.length; i++) {
+//     th = document.createElement("th");
+//     th.innerHTML = daysOfWeek[i];
+//     header.appendChild(th);
+//   }
+  
+//   // this adds a day onto February every leap year (every 4 years)
+//   let daysInMonth = new Date(2019, parseInt(month) + 1, 0).getDate();
+//   if(month == 1 && year % 4 == 0) {
+//    daysInMonth = daysInMonth + 1;
+//   }
+  
+//   // define a start day, then loop through the month
+//   let startDay = new Date(year, month, 1).getDay();
+//   let weekIndex = cal.getElementsByTagName("tr").length;
+//   let day = startDay == 0 ? 7 : startDay;
+//   var row = cal.insertRow(weekIndex);
 
-renderCal(getNumberOfDays(yearChosen, monthChosen));
+//   // creates the blanks at the start of the month
+//   for (let ed = 1; ed < day; ed++) {
+//     row.insertCell(-1);
+//   }
+
+//   // loops through the days adding cells and creating new rows
+//   for (let dim = 1; dim <= daysInMonth; dim++) {
+//     let cell = row.insertCell(-1);
+//     let gbFormat = new Intl.DateTimeFormat("en-GB").format(new Date(year, month, dim));
+//     cell.setAttribute("data-date-intl", new Date(year, month, dim));
+//     cell.setAttribute("data-date", gbFormat);
+    
+//     if(disabledDays.includes(gbFormat)) {
+//        cell.classList.add('disabled');
+//     }
+    
+//     cell.innerHTML = '<span>'+dim+'</span>';
+//     day++;
+//     if (day == 8) {
+//       weekIndex++;
+//       row = cal.insertRow(weekIndex);
+//       day = 1;
+//     }
+//   }
+//   let monthTitleCont = document.createElement("div");
+//   monthTitleCont.appendChild(monthTitle);
+//   if(mode == "single") {
+//     monthControls.appendChild(lArrow);
+//     monthControls.appendChild(monthTitleCont);
+//     monthControls.appendChild(rArrow);
+//   } else {
+//     monthControls.appendChild(monthTitleCont); 
+//   }
+//   newMonth.appendChild(cal);
+//   calContainer.appendChild(newMonth);
+//   if (month == new Date().getMonth() && year == new Date().getFullYear()) {
+//     highlightToday();
+//   }
+  
+//   // function to add event listeners to each table cell
+//   initCells(cal);
+// }
+
+// // function to create and append more months to the calendar
+// function addMoreMonths() {
+//   let moreMonths = document.createElement("div");
+//   moreMonths.classList.add("more__months");
+//   moreMonths.innerHTML = "+";
+//   moreMonths.addEventListener("click", function() {
+//     for (let i = 1; i <= 3; i++) {
+//       month = month + 1;
+//       if (month == 12) {
+//         month = 0;
+//         year = year + 1;
+//       }
+//       buildCal();
+//     }
+//     let moreMonthsCurrent = document.getElementsByClassName("more__months")[0];
+//     moreMonthsCurrent.remove();
+//     calContainer.appendChild(moreMonths);
+//   });
+//   calContainer.appendChild(moreMonths);
+// }
+
+// /* function to build a table */
+// function createTable() {
+//   createTable.innerHTML = `
+//   <style>
+//   table.circles {
+//     height: 280px;
+//     width: 280px;
+//     margin: 0 auto;
+//   }
+
+//   thead th {
+//       text-transform: capitalize;
+//       font-size: 18px;
+//       margin: 0;
+//   }
+
+//   tbody tr td {
+//       font-size: 20px;
+//       text-align: center;
+//   }
+//   </style>
+//   `
+//   let table = document.createElement("table");
+//   table.style.height = '280px';
+//   table.style.width = '280px';
+//   let thead = table.createTHead();
+//   let tbody = table.createTBody();
+//   table.classList.add(style)
+//   return table;
+// }
+
+// var startDateSelected, endDaySelected = false;
+// var startDay, endDay, cells, rangeSelected;
+
+// function initCells(newMonth) {
+//   cells = newMonth.querySelectorAll("[data-date]");
+//   // loop through all cells adding event listeners
+//   cells.forEach(function(item, idx) {
+//     item.addEventListener("click", function() {
+//       /* 
+//       if the start date hasn't been selected
+//       get the start date, set it and highlight it 
+//       */
+//       if (!startDateSelected || typeof startDateSelected == "undefined") {
+//         clearRange();
+//         endInput.value = "";
+//         startDay = item.getAttribute("data-date");
+//         item.classList.add('selected__startEnd');
+//         dateRange.push(startDay);
+//         rangeStart = startDay;
+//         startInput.value = startDay;
+//         endDay = false;
+//         startDateSelected = true;
+//         rangeSelected = false;
+//       } else {
+//         // when the user clicks the end date, set the date and highlight
+//         endDay = this.getAttribute("data-date");
+//         let selected = false;
+//         /* 
+//         loop through the days between the start and end day
+//         highlight each cell until you reach the end date
+//         processDate turns the en-GB date format back to standard format to compare 
+//         */
+//         if (processDate(endDay) > processDate(startDay)) {
+//           rangeEnd = endDay;
+//           endInput.value = endDay;
+//           item.classList.add('selected__startEnd');
+//           while (selected == false) {
+//             if (startDay !== endDay) {
+//               var dateParts = startDay.split("/");
+//               let tomorrow = new Date(
+//                 dateParts[2],
+//                 dateParts[1] - 1,
+//                 parseInt(dateParts[0]) + 1
+//               );
+//               let tempDate = new Intl.DateTimeFormat("en-GB").format(tomorrow);
+//               startDay = tempDate;
+//               rangeSelected = true;
+//             } else {
+//               selected = true; 
+//               logDateArray();
+//             }
+//           }
+//           startDateSelected = false;
+//         } else {
+//           /* 
+//           if the user selects an earlier start date:
+//           clear the range
+//           set the new start date
+//           colour the new start date 
+//           */
+//           clearRange();
+//           startDay = item.getAttribute("data-date");
+//           startInput.value = startDay;
+//           item.classList.add('selected__startEnd');
+//           endDay = false;
+//           startDateSelected = true;
+//           rangeSelected = false;
+//         }
+//       }
+//     });
+//   });
+// }
+
+// /*
+// This function saves the selected dates into an array
+// The saved array is used to render the selected dates
+// if the month changes.
+// */
+// function logDateArray() {
+//   dateRange = [];
+//   let start = startInput.value;
+//   let end = endInput.value;
+//   let tempDate = start;
+//   while(tempDate !== endInput.value) {
+//     var dateParts = tempDate.split("/");
+//     let tomorrow = new Date(
+//       dateParts[2],
+//       dateParts[1] - 1,
+//       parseInt(dateParts[0]) + 1
+//     );
+//     tempDate = new Intl.DateTimeFormat("en-GB").format(tomorrow);
+//     dateRange.push(tempDate);
+//   }
+//   dateRange.unshift(start);
+//   colourDates();
+// }
+
+// /*
+// Function to loop through the saved array and colour the dates
+// */
+// function colourDates() {
+//   dateRange.forEach(function(itm, idx) {
+//     let classToAdd = idx == 0 || idx == dateRange.length - 1 ? 'selected__startEnd' : 'selected';
+//     let cell = document.querySelectorAll('[data-date="'+itm+'"]')[0];
+//     if(typeof cell != "undefined") {
+//       cell.classList.add(classToAdd);
+//     }
+//   });
+// }
+
+// /*
+// Process date turns en-GB date format back to a standard date object
+// */
+// function processDate(date) {
+//   var parts = date.split("/");
+//   return new Date(parts[2], parts[1] - 1, parts[0]);
+// }
+
+// /* 
+// this function deletes all the <tr> elements
+// then rebuilds the table header
+// */
+// function deletePrevMonth() {
+//   const cal = document.getElementById("cal");
+//   cal.innerHTML = "";
+// }
+
+// /* 
+// function highlights the current day we're on
+// only if we're on the right month
+// */
+// function highlightToday() {
+//   let today = new Date();
+//   let todayCell = document.querySelector(
+//     'td[data-date="' + new Intl.DateTimeFormat("en-GB").format(today) + '"]'
+//   );
+//   todayCell.style.backgroundColor = "#ededed";
+// }
+
+// /* function to clear the selected dates */
+// function clearRange() {
+//   let selected = document.querySelectorAll('.selected,.selected__startEnd');
+//   for(let item of selected) {
+//     item.classList = "";
+//   }
+//   dateRange = [];
+// }
